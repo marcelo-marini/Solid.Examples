65646cd1d4c0bbcd32e088a7ce25d396ef0dd86d
Marcelo Marini <marcelomarini17@hotmail.com>
2019-02-28 01:29:04 -0300

    - LSP

---
+++ Solid.Examples/3 - LSP/Violacao/Rectangle.cs
@@ -0,0 +1,8 @@
+﻿namespace Solid.Examples._3___LSP.Violacao {
+    public class Rectangle {
+
+        public virtual double Height { get; set; }
+        public virtual double Width { get; set; }
+        public double Area { get { return Height * Width; } }
+    }
+}
